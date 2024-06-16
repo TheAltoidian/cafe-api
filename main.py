db7c34998477c82af4513487fc4b441d97c63f87
Cullen <cullenjcasey@gmail.com>
2024-06-16 13:44:13 -0700

    can get a randomized cafe, or a list of all cafes

--- main.py
+++ main.py
@@ -2,25 +2,15 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
-
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from random import randint
 
 app = Flask(__name__)
 
 # CREATE DB
 class Base(DeclarativeBase):
     pass
+
+
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy(model_class=Base)
@@ -46,12 +36,52 @@ with app.app_context():
     db.create_all()
 
 
+def to_dict(self):
+    return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+def json_cafe(cafe):
+    return jsonify(cafe={
+        "name": cafe.name,
+        "map_url": cafe.map_url,
+        "img_url": cafe.img_url,
+        "location": cafe.location,
+        # "id": cafe.id,
+        "amenities": {
+           "seats": cafe.seats,
+           "has_toilet": cafe.has_toilet,
+           "has_wifi": cafe.has_wifi,
+           "has_sockets": cafe.has_sockets,
+           "can_take_calls": cafe.can_take_calls,
+           "coffee_price": cafe.coffee_price
+            }
+        }
+    )
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
 # HTTP GET - Read Record
+@app.route("/random", methods=["GET"])
+def random_cafe():
+    cafe_length = len(db.session.execute(db.select(Cafe.id)).all())
+    cafe = db.get_or_404(Cafe, randint(1, cafe_length))
+    print(cafe)
+    jsonified_cafe = json_cafe(cafe)
+    print(jsonified_cafe)
+    return jsonified_cafe
+
+
+@app.route("/all", methods=["GET"])
+def all_cafes():
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafes = result.scalars().all()
+    #This uses a List Comprehension but you could also split it into 3 lines.
+    return jsonify(cafes=[to_dict(cafe) for cafe in all_cafes])
+
 
 # HTTP POST - Create Record
 
